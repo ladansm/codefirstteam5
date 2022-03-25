dc63250a9aca6c79a05ffebfba85fe5b793516e1
HanitraMelodie <Hanitra_Melodie@hotmail.fr>
2022-03-25 20:16:22 +0000

    Add javascript function to erase html content

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // blablabla
+function test(event){
+    event.preventDefault(event);
+    let forecast=document.querySelector("#welcomeAndForm")
+    forecast.innerHTML="<div>BLABLABLA<div/>"
+
+}
+let searchbutton=document.querySelector("#search-button")
+searchbutton.addEventListener("click",test)
+
