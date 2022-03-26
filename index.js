9d420278923d65e78acc206eb8a8dd32bee39258
HANITRA RAKOTOARIMANANA <hanitrar5@gmail.com>
2022-03-26 22:30:36 +0000

    city input go to the forecast form with javascript function test

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 function test(event){
     event.preventDefault(event);
     let forecast=document.querySelector("#welcomeAndForm")
-    forecast.innerHTML=` <h3>Thank you for booking your break with us</h3>
+    let cityInput=document.querySelector('#city-input')
+   
+    let city=cityInput.value
+   
+    forecast.innerHTML=`<h3>Thank you for booking your break with us</h3>
     <h4>Summary</h4>
     <div id="date">Date</div>
-    <div id="location">Location</div>
+    <div id="location">${city}</div>
     <div id="temp">Temperature</div>`
 }
 let searchbutton=document.querySelector("#book-button")
