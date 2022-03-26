fa3f580c62089adbb91b558e415fbcbdd9cd781e
HanitraMelodie <Hanitra_Melodie@hotmail.fr>
2022-03-26 23:33:20 +0000

    adding javascript content for date/temperature/weather

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ function test(event){
     event.preventDefault(event);
     let forecast=document.querySelector("#welcomeAndForm")
     let cityInput=document.querySelector('#city-input')
-   
     let city=cityInput.value
-   
+    let tempInput=document.querySelector('#temp-input')
+    let temp=tempInput.value
+    let dateInput=document.querySelector('#date-input')
+    let date=dateInput.value
+    let weatherInput=document.querySelector('#weather-input')
+    let weather=weatherInput.value
+    
     forecast.innerHTML=`<h3>Thank you for booking your break with us</h3>
     <h4>Summary</h4>
-    <div id="date">Date</div>
+    <div id="date">${date}</div>
     <div id="location">${city}</div>
-    <div id="temp">Temperature</div>`
+    <div id="weather">${weather}</div>
+    <div id="temp">${temp}</div>`
+
 }
 let searchbutton=document.querySelector("#book-button")
 searchbutton.addEventListener("click",test)
