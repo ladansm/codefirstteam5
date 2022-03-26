9eb350dd3fb7bf8fe93ab6a21da2de361c42850e
HANITRA RAKOTOARIMANANA <hanitrar5@gmail.com>
2022-03-26 16:46:44 +0000

    add javascript form + dropdownlist workking in html for weather description

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 function test(event){
     event.preventDefault(event);
     let forecast=document.querySelector("#welcomeAndForm")
-    forecast.innerHTML="<div>BLABLABLA<div/>"
-
+    forecast.innerHTML=` <h3>Thank you for booking your break with us</h3>
+    <h4>Summary</h4>
+    <div id="date">Date</div>
+    <div id="location">Location</div>
+    <div id="temp">Temperature</div>`
 }
-let searchbutton=document.querySelector("#search-button")
+let searchbutton=document.querySelector("#book-button")
 searchbutton.addEventListener("click",test)
 
