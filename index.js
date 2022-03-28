735434705bf57523e0477a79c7ba6e5f48c5f782
yusuf3a50 <ybyphone@disroot.org>
2022-03-29 00:31:06 +0100

    add form validation

--- index.js
+++ index.js
@@ -2,16 +2,32 @@
 function makeForecast(event){
     event.preventDefault(event);    //this prevents the new HTML from immediately disappearing
     let forecast=document.querySelector("#welcomeAndForm")      //this is the section of HTML that will be rewritten
-    let cityInput=document.querySelector('#city-input')
-    let city=cityInput.value
-    let tempInput=document.querySelector('#temp-input')
-    let temp=tempInput.value
-    let dateInput=document.querySelector('#date-input')
-    let date=dateInput.value
-    let weatherInput=document.querySelector('#weather-input')
-    let weather=weatherInput.value
+    
+    let city = document.forms["mainForm"]["city-input"].value;
+    if (city == "") {
+    alert("Location must be filled out");
+    return false;
+    }
+    
+    let weather = document.forms["mainForm"]["weather-input"].value;
+    if (weather == "") {
+    alert("Weather must be filled out");
+    return false;
+    }
+    
+    let temp = document.forms["mainForm"]["temp-input"].value;
+    if (temp == "") {
+    alert("Temperature must be filled out with a number");
+    return false;
+    }
 
-    //this is what the section will be rewritten with
+    let date = document.forms["mainForm"]["date-input"].value;
+    if (date == "") {
+    alert("Date must be filled out");
+    return false;
+    }
+
+    //this is what the HTML will be rewritten with
     forecast.innerHTML = `<h3>Thank you for booking your break with us</h3>
     <h4>Summary</h4>
     <div id="date">The weather on ${date}</div>
