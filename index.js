863c3cb5c824ced2e8ea0ec4d0558de8795afd00
yusuf3a50 <ybyphone@disroot.org>
2022-03-29 09:31:27 +0100

    improve indentation

--- index.js
+++ index.js
@@ -5,35 +5,36 @@ function makeForecast(event){
     
     let city = document.forms["mainForm"]["city-input"].value;
     if (city == "") {
-    alert("Location must be filled out");
-    return false;
+        alert("Location must be filled out");
+        return false;
     }
     
     let weather = document.forms["mainForm"]["weather-input"].value;
     if (weather == "") {
-    alert("Weather must be filled out");
-    return false;
+        alert("Weather must be filled out");
+        return false;
     }
     
     let temp = document.forms["mainForm"]["temp-input"].value;
     if (temp == "") {
-    alert("Temperature must be filled out with a number");
-    return false;
+        alert("Temperature must be filled out with a number");
+        return false;
     }
 
     let date = document.forms["mainForm"]["date-input"].value;
     if (date == "") {
-    alert("Date must be filled out");
-    return false;
+        alert("Date must be filled out");
+        return false;
     }
 
     //this is what the HTML will be rewritten with
     forecast.innerHTML = `<h3>Thank you for booking your break with us</h3>
-    <h4>Summary</h4>
-    <div id="date">The weather on ${date}</div>
-    <div id="location">In ${city}</div>  
-    <div id="weather">The sky will have: ${weather}</div>
-    <div id="temp">With a temperature of: ${temp}&deg;C</div>`
+        <h4>Summary</h4>
+        <div id="date">The weather on ${date}</div>
+        <div id="location">In ${city}</div>  
+        <div id="weather">The sky will have: ${weather}</div>
+        <div id="temp">With a temperature of: ${temp}&deg;C</div>`
+
 };
 
 //add an event listener that will listen for the click of bookButton and then execute function makeForecast()
